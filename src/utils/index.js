d3aadc1e415cb128e854e631985681a086d03a49
knm070 <nodirqurbanov0770@gmail.com>
2024-10-25 17:57:48 +0500

    Expert detail page

--- src/utils/index.js
+++ src/utils/index.js
@@ -2,6 +2,7 @@ import { lazy } from 'react';
 const Main = lazy(() => import('@/pages/main'));
 const Login = lazy(() => import('@/pages/Login'));
 const Experts = lazy(() => import('@/pages/Experts'));
+const ExpertsSingle = lazy(() => import('@/pages/ExpertsSingle'));
 export const Data = [
 	{
 		id: 1,
@@ -17,5 +18,10 @@ export const Data = [
 		id: 3,
 		path: '/experts',
 		component: Experts,
+	},
+	{
+		id: 4,
+		path: '/experts/single',
+		component: ExpertsSingle,
 	},
 ];
